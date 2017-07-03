62ca300bafa761a63bb1411505b4d7c48acaba9a
James <james.vogon@gmail.com>
2017-07-03 03:48:35 +0000

    remove @ mentions to blocked instances from home timeline

--- app/models/status.rb
+++ app/models/status.rb
@@ -76,6 +76,7 @@ class Status < ApplicationRecord
   scope :including_silenced_accounts, -> { left_outer_joins(:account).where(accounts: { silenced: true }) }
   scope :not_excluded_by_account, ->(account) { where.not(account_id: account.excluded_from_timeline_account_ids) }
   scope :not_domain_blocked_by_account, ->(account) { account.excluded_from_timeline_domains.blank? ? left_outer_joins(:account) : left_outer_joins(:account).where('accounts.domain IS NULL OR accounts.domain NOT IN (?)', account.excluded_from_timeline_domains) }
+  scope :not_domain_blocked_by_server, ->(domain_table) { joins(mentions: :account).joins("inner join #{domain_table} on #{domain_table}.domain = accounts.domain") }
 
   scope :not_local_only, -> { where(local_only: [false, nil]) }
 
@@ -185,7 +186,7 @@ class Status < ApplicationRecord
     end
 
     def as_home_timeline(account)
-      where(account: [account] + account.following).where(visibility: [:public, :unlisted, :private])
+      not_domain_blocked_by_server(AllowDomainService.record_type.table_name).where(account: [account] + account.following).where(visibility: [:public, :unlisted, :private])
     end
 
     def as_direct_timeline(account)
